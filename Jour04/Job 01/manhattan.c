d7eb0683cfbf48f82e5a313242423e6dc27d975d
louis-cordier <louis.cordier@laplateforme.io>
2026-01-10 13:15:00 +0100

    feat: added last jobs

--- Jour04/Job 01/manhattan.c
+++ Jour04/Job 01/manhattan.c
@@ -1,31 +1,59 @@
+// // #include "manhattan.h"
+
+// // int manhattan(point a, point b) {
+// //     return ABS(a.x - b.x) + ABS(a.y - b.y);
+// // }
+
+// #include <stdio.h>
 // #include "manhattan.h"
 
-// int manhattan(point a, point b) {
+
+// int manhattan(Point a, Point b) {
+
 //     return ABS(a.x - b.x) + ABS(a.y - b.y);
+// }
+// // {
+// //     printf("%d %d %d %d\n", a.x, a.y, b.x, b.y);
+// //     // int ret = ABS (a.x - b.x) + ABS (a.y - b.y);
+// //     int dx = ABS(a.x - b.x);
+// //     int dy = ABS(a.y - b.y);
+// //     return dx + dy;
+// // }
+
+// int main(void) {
+//     // Point p1, p2;
+//     Point p1 = {0, 0};
+//     Point p2 = {3, 4};
+//     int result = manhattan(p1, p2);
+
+//     printf("%d\n",manhattan(p1, p2));
+//     return 0;
 // }
 
 #include <stdio.h>
 #include "manhattan.h"
 
 
-int manhattan(point a, point b) {
 
-    return ABS(a.x - b.x) + ABS(a.y - b.y);
+
+
+int manhattan(Point a, Point b){
+ printf("%d %d %d %d\n ", a.x, b.x, a.y, b.y);
+
+ int dx= ABS(a.x-b.x);
+ int dy= ABS(a.y-b.y);
+
+ return  dy+dx; 
+ 
 }
-// {
-//     printf("%d %d %d %d\n", a.x, a.y, b.x, b.y);
-//     // int ret = ABS (a.x - b.x) + ABS (a.y - b.y);
-//     int dx = ABS(a.x - b.x);
-//     int dy = ABS(a.y - b.y);
-//     return dx + dy;
-// }
 
-int main(void) {
-    // Point p1, p2;
-    Point p1 = {0, 0};
-    Point p2 = {3, 4};
-    int result = manhattan(p1, p2);
+int main(){
+  
+  Point p1 = {1,2};
+  Point p2 = {3,4};
 
-    printf("%d\n",manhattan(p1, p2));
-    return 0;
+  printf("%d\n",manhattan(p1,p2));
+
+
+  return 0;
 }
--- Jour04/Job 01/manhattan.h
+++ Jour04/Job 01/manhattan.h
@@ -1,27 +1,44 @@
+// // #ifndef MANHATTAN_H
+// // #define MANHATTAN_H
+
+// // #define ABS(x) ((x) < 0 ? -(x) : (x))
+
+// // typedef struct {
+// //     int x;
+// //     int y;
+// // } point;
+
+// // int manhattan(point a, point b);
+
+// // #endif // MANHATTAN_H
 // #ifndef MANHATTAN_H
 // #define MANHATTAN_H
 
-// #define ABS(x) ((x) < 0 ? -(x) : (x))
+// #define ABS (x) ((x) > 0 ? (x) : -(x))
+// // #define ABS_DIFF(x, y) ((x - y))
 
-// typedef struct {
+// typedef struct Point {
 //     int x;
 //     int y;
-// } point;
+// } Point;
 
-// int manhattan(point a, point b);
+// int manhattan(Point a, Point b);
 
-// #endif // MANHATTAN_H
+// #endif
+
 #ifndef MANHATTAN_H
 #define MANHATTAN_H
 
-#define ABS (x) ((x) > 0 ? (x) : -(x))
-// #define ABS_DIFF(x, y) ((x - y))
+#define ABS(x) ((x>0? x: -(x)))
+//#define ABS_DIFF (x,y) ((x-y)>0; (x-y); (y-x))
 
-typedef struct point {
-    int x;
-    int y;
-} point;
+typedef struct{
+  int x;
+  int y;
+}Point;
 
-int manhattan(point a, point b);
+int manhattan(Point a, Point b);
+
+
 
 #endif
